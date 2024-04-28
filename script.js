48d84085de8c8dcef5510c5e3466f9598093b29c
/ A N K I T / <116625135+xnkit69@users.noreply.github.com>
2024-04-28 14:13:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -84,19 +84,11 @@ const questions = [
       { text: '22', correct: false }
     ]
   },
-  {
-    question: 'Who is the best YouTuber?',
-    answers: [
-      { text: 'Web Dev Simplified', correct: true },
-      { text: 'Traversy Media', correct: true },
-      { text: 'Dev Ed', correct: true },
-      { text: 'Fun Fun Function', correct: true }
-    ]
-  },
+
   {
     question: 'Is web development fun?',
     answers: [
-      { text: 'Kinda', correct: false },
+      { text: 'Kind', correct: false },
       { text: 'YES!!!', correct: true },
       { text: 'Um no', correct: false },
       { text: 'IDK', correct: false }
